768ce22f1a067becdb1a186b6485f0050a187d6b
chachen <chachen@expedia.com>
2017-09-24 15:55:17 +0800

    Add save and findall for parking places

---
+++ src/main/java/com/jiufeng/car/controller/CompareController.java
@@ -0,0 +1,50 @@
+package com.jiufeng.car.controller;
+
+import com.jiufeng.car.dao.IParkingLotDao;
+import com.jiufeng.car.entity.ParkingLot;
+import com.jiufeng.car.util.JsonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by Chen Chao on 3/17/2017.
+ */
+@RestController
+public class CompareController {
+
+    @Autowired
+    IParkingLotDao parkingLotDao;
+
+
+    @RequestMapping
+            (value = {"/park/save",},
+                    method = {RequestMethod.POST},
+                    produces = {MediaType.APPLICATION_JSON_VALUE},
+                    consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<?> saveParkingLot(@RequestHeader HttpHeaders requestHeader,
+                                            @RequestBody String requestBody) throws Exception {
+        ParkingLot parkingLot = JsonUtil.fromJson(requestBody, ParkingLot.class);
+        parkingLotDao.save(parkingLot);
+        ResponseEntity responseEntity = new ResponseEntity(HttpStatus.OK);
+        return responseEntity;
+    }
+
+
+    @RequestMapping
+            (value = {"/park/findall",},
+                    method = {RequestMethod.POST},
+                    produces = {MediaType.APPLICATION_JSON_VALUE},
+                    consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<?> findAllParkingPlaces(@RequestHeader HttpHeaders requestHeader) throws Exception {
+        List<ParkingLot> parkingLotList = parkingLotDao.findAll();
+        ResponseEntity responseEntity = new ResponseEntity(parkingLotList, HttpStatus.OK);
+        return responseEntity;
+    }
+
+}
---
+++ src/main/java/com/jiufeng/car/util/JsonUtil.java
@@ -0,0 +1,78 @@
+/**
+ * JsonUtil.java
+ * <p>
+ * Copyright 2015 Expedia, Inc. All rights reserved.
+ * EXPEDIA PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.jiufeng.car.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+
+import java.io.IOException;
+
+/**
+ * Json util class - marshal/unmarshal json format request and response etc.
+ *
+ * @author <a href="mailto:mixie@expedia.com">Ming Xie</a>
+ */
+public class JsonUtil
+{
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    static
+    {
+
+        objectMapper.setPropertyNamingStrategy(new com.fasterxml.jackson.databind.PropertyNamingStrategy.PascalCaseStrategy());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JodaModule());
+    }
+
+    private JsonUtil()
+    {
+    }
+
+    public static <T> T fromJson(String request, Class<T> clazz)
+            throws IOException
+    {
+        if (request == null)
+        {
+            return null;
+        }
+
+        return objectMapper.readValue(request, clazz);
+    }
+
+    public static String toJson(Object response)
+            throws JsonProcessingException
+    {
+        return objectMapper.writeValueAsString(response);
+    }
+
+    public static String toJsonWithoutException(Object object)
+    {
+        String jsonString;
+        try
+        {
+            jsonString = toJson(object);
+        }
+        catch (JsonProcessingException e)
+        {
+            jsonString = "Failed to get pretty printed JSON string. " + e.getMessage();
+        }
+        return jsonString;
+    }
+
+    public static String getHTTPContextString(String endpoint, Object request, Object response)
+    {
+        return System.lineSeparator() + "Endpoint:" +
+                System.lineSeparator() + endpoint +
+                System.lineSeparator() + "Request:" +
+                System.lineSeparator() + toJsonWithoutException(request) +
+                System.lineSeparator() + "Response:" +
+                System.lineSeparator() + toJsonWithoutException(response);
+    }
+}
