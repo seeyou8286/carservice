c8482250802972fabefb1eedf892e53efadc197c
chachen <chachen@expedia.com>
2017-09-28 13:40:09 +0800

    Update README

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # carservice
-
+=========================
 This is a web service to do CRUD for MongoDB in Alibaba Cloud
 
 
-To access swagger-ui
+
+MongoDB
+=========================
+The current database we are using is MongoDB in Alibaba Clou.
+
+You can visit from 
+Host IP: 119.23.224.66
+Port:27017
+
+How to run
+=========================
+1. mvn clean install
+2. java -jar car-service-1.0-SNAPSHOT.jar
+
+APIs
+=========================
+To access swagger-uiswagger-ui
 http://localhost:8080/swagger-ui.html
+
--- src/test/java/com/jiufeng/car/dao/impl/ParkingLotDaoTest.java
+++ src/test/java/com/jiufeng/car/dao/impl/ParkingLotDaoTest.java
@@ -1,23 +1,28 @@
-package com.jiufeng.car.dao;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-/**
- * Created by chachen on 9/24/2017.
- */
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ParkingLotDaoTest {
-
-    @Autowired
-    private IParkingLotsDao citiesDao;
-
-    @Test
-    public void testSaveUser() throws Exception {
+//package com.jiufeng.car.dao;
+//
+//import com.jiufeng.car.entity.ParkingLot;
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * Created by chachen on 9/24/2017.
+// */
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class ParkingLotDaoTest {
+//
+//    @Autowired
+//    private IParkingLotsDao citiesDao;
+//
+//    @Test
+//    public void testSaveUser() throws Exception {
 //        ParkingLot parkingLot = new ParkingLot();
 //        parkingLot.setCityName("深圳");
 //        List<String> airport = new ArrayList();
@@ -28,10 +33,10 @@ public class ParkingLotDaoTest {
 //        placeNames.add("深圳大宝停车场");
 //        parkingLot.setPlaceName(placeNames);
 //        citiesDao.save(parkingLot);
-    }
-
-    @Test
-    public void testUpdateUser() throws Exception {
+//    }
+//
+//    @Test
+//    public void testUpdateUser() throws Exception {
 //        ParkingLot parkingLot = new ParkingLot();
 //        parkingLot.setCityName("深圳");
 //        List<String> airport = new ArrayList();
@@ -42,16 +47,16 @@ public class ParkingLotDaoTest {
 //        placeNames.add("深圳很棒停车场");
 //        parkingLot.setPlaceName(placeNames);
 //        citiesDao.updateByCityName(parkingLot);
-    }
-
-    @Test
-    public void findAll()
-    {
+//    }
+//
+//    @Test
+//    public void findAll()
+//    {
 //        List<ParkingLot> parkingLots = citiesDao.findAll();
 //        System.out.println(parkingLots);
-    }
-
-
-
-
-}
+//    }
+//
+//
+//
+//
+//}
