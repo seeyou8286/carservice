c8943c5aaaaec24f5c2af7338cbf89fd0f051db6
chachen <seeyou8286@163.com>
2017-10-15 22:50:04 +0800

    Add first letter

--- src/main/java/com/jiufeng/car/entity/Airport.java
+++ src/main/java/com/jiufeng/car/entity/Airport.java
@@ -10,6 +10,7 @@ import java.io.Serializable;
 public class Airport implements Serializable {
     private static final long serialVersionUID = -3258839839160856613L;
     private String name;
+    private String firstLetter;
 
     public Airport(String name) {
         this.name = name;
